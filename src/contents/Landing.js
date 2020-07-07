c4d6ec8e6830de86c1adcf499d95984fc53e9742
Alex Feeley <alexandra.feeley.lamb@vanderbilt.edu>
2020-07-07 15:58:19 -0400

    Added animated background to landing page

--- src/contents/Landing.js
+++ src/contents/Landing.js
@@ -1,19 +1,40 @@
-import React, { Component } from "react";
-import { Jumbotron, Container, Button } from "react-bootstrap";
+import React, { Component } from 'react';
+import BIRDS from 'vanta/dist/vanta.birds.min';
+import { Jumbotron, Container, Button } from 'react-bootstrap';
 
-// import BIRDS from '../../vanta/dist/vanta.birds.min';
-
-class About extends Component {
+class Landing extends Component {
+    constructor() {
+        super()
+        this.vantaRef = React.createRef()
+    }
+    componentDidMount() {
+        this.vantaEffect = BIRDS({
+            el: this.vantaRef.current,
+            mouseControls: true,
+            touchControls: true,
+            backgroundColor: 0x282828,
+            color1: 0x350052,
+            color2: 0x123c69,
+            colorMode: "lerp",
+            birdSize: 1.50,
+            quantity: 3.5,
+            separation: 15,
+            cohesion: 50
+        })
+    }
+    componentWillUnmount() {
+        if (this.vantaEffect) this.vantaEffect.destroy()
+    }
     render() {
         return (
-            <Jumbotron fluid>
+            <Jumbotron fluid ref = {this.vantaRef}>
                 <Container className = "align-content-center">
                     <h1 className = "lead">Hi, I'm</h1>
                     <h1 className = "display-3">Alex Feeley.</h1>
                     <h1 className = "lead">I'm a Junior at Vanderbilt studying Computer Engineering with a 
-                        passion for creating neat things.</h1>
+                        passion for creating cool things.</h1>
                     <p>
-                        <Button>Want to learn more?</Button>
+                        <Button href = "#about">Want to learn more?</Button>
                     </p>
                 </Container>
             </Jumbotron>
@@ -21,4 +42,4 @@ class About extends Component {
     }
 }
 
-export default About; 
+export default Landing; 
