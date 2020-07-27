8291bfe177c8f396c82eec23b27e49545cb43d94
Alex Feeley <alexandra.feeley.lamb@vanderbilt.edu>
2020-07-27 19:15:54 -0400

    Changed background animation for landing page

--- src/contents/About.js
+++ src/contents/About.js
@@ -2,16 +2,6 @@
 import React from 'react';
 // import { Jumbotron, Container, Button } from "react-bootstrap";
 
-// import BIRDS from '../../vanta/dist/vanta.birds.min';
-
-// class About extends Component {
-//     render() {
-//         return (
-//             <h1> Hello, World! </h1>
-//         );
-//     }
-// }
-
 const About = () => {
     return (
         <div>
